3d7af01fdec6b2901ae3305a5e66251878584f91
Omoyemi Olukoya <yemmie69@hotmail.com>
2020-05-07 15:07:03 +0100

    update commit

---
+++ security.groups.tf
@@ -0,0 +1,31 @@
+resource "aws_security_group" "allow" {
+  name        = "allow"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "${aws_vpc.jenkins.id}"
+
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "weather_jenkins"
+  }
+}
